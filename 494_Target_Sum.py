06dc709e2a1fa48626fd1e91b5af69ef1b3d1899
Administrator <changyuan@ebupt.com>
2019-08-30 18:02:41 +0800

    first

--- 494_Target_Sum.py
+++ 494_Target_Sum.py
@@ -1,51 +1,45 @@
-nums = [1, 1, 1, 1, 1]
-S = 3
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        if len(nums)==1:
+            if nums[0]==S or -nums[0]==S:
+               return 1
+            else:
+               return 0
+        graph = {}
+        for index in range(2 * len(nums)):
+            if index % 2 == 0:
+                graph[index] = [index + 2, index + 3]
+            elif index % 2 == 1:
+                graph[index] = [index + 1, index + 2]
 
-graph = {}
-for index in range(2*len(nums)):
-    if index % 2 == 0:
-        graph[index] = [index + 2, index + 3]
-    elif index % 2 == 1:
-        graph[index] = [index + 1, index + 2]
+        graph[2 * len(nums) - 1] = []
+        graph[2 * len(nums) - 2] = []
 
+        count = 0
+        stack = [(0, [0], -nums[0])]
+        while stack:
+            index, path, total = stack.pop()
+            for i in set(graph[index]) - set(path):
+                if i % 2 == 0:
+                    new_total = total - nums[i // 2]
+                    stack.append((i, path + [i], new_total))
+                elif i % 2 == 1:
+                    new_total = total + nums[i // 2]
+                    stack.append((i, path + [i], new_total))
+                if new_total == S and len(path + [i]) == len(nums):
+                    count += 1
+        stack.clear()
 
-graph[2*len(nums)-1]=[]
-graph[2*len(nums)-2]=[]
-print(graph)
-
-count = 0
-stack = [(0, [0], -nums[0])]
-while stack:
-    index, path, total = stack.pop()
-    print(index,path,total)
-    for i in set(graph[index]) - set(path):
-        print(set(graph[index]) - set(path))
-        print(i)
-        if i % 2 == 0:
-            total = total - nums[i // 2]
-            stack.append((i, path + [i], total))
-        elif i % 2 == 1:
-            total = total + nums[i // 2]
-            stack.append((i, path + [i], total))
-        if total == S:
-            count += 1
-# stack.clear()
-#
-# stack = [(1, [1], nums[0])]
-# while stack:
-#     index, path, total = stack.pop()
-#     for i in set(graph[index]) - set(path):
-#         if i % 2 == 0:
-#             total = total - nums[i // 2]
-#             stack.append((i, path + [i], total))
-#         elif i % 2 == 1:
-#             total = total + nums[i // 2]
-#             stack.append((i, path + [i], total))
-#         if total == S:
-#             count += 1
-print(count)
-
-# a=[1,2]
-# b=[1,3,4]
-# for i in set(a)-set(b):
-#     print(i)
+        stack = [(1, [1], nums[0])]
+        while stack:
+            index, path, total = stack.pop()
+            for i in set(graph[index]) - set(path):
+                if i % 2 == 0:
+                    new_total = total - nums[i // 2]
+                    stack.append((i, path + [i], new_total))
+                elif i % 2 == 1:
+                    new_total = total + nums[i // 2]
+                    stack.append((i, path + [i], new_total))
+                if new_total == S and len(path + [i]) == len(nums):
+                    count += 1
+        return count
