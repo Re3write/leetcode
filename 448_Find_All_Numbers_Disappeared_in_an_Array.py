463c5f48ddc334c18a5c7def2934c7e974c63ec5
changyuan <1936929110@qq.com>
2019-09-01 19:57:08 +0800

    ‘消失的数’

--- 448_Find_All_Numbers_Disappeared_in_an_Array.py
+++ 448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -14,38 +14,27 @@ def findDisappearedNumbers(nums):
     # 使用set
     return list(set(range(1, len(nums) + 1)) - set(nums))
 
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        nums=[0]+nums
-        n=len(nums)
-        for i in range(1,n):
-            while nums[i]!=i:
-                nums[nums[i]],nums[i]=nums[i],nums[nums[i]]
-                if nums[nums[i]]==nums[i]:
-                    break
-        return [i for i in range(1,n) if nums[i]!=i]
-
-    # 扫描位置（i），交换次数，原数组
-    # 0
-    # 0[0, 4, 3, 2, 7, 8, 2, 3, 1]
-    # 1
-    # 1[0, 7, 3, 2, 4, 8, 2, 3, 1]
-    # 1
-    # 2[0, 3, 3, 2, 4, 8, 2, 7, 1]
-    # 1
-    # 3[0, 2, 3, 3, 4, 8, 2, 7, 1]
-    # 1
-    # 4[0, 3, 2, 3, 4, 8, 2, 7, 1]
-    # 5
-    # 5[0, 3, 2, 3, 4, 1, 2, 7, 8]
-    # 5
-    # 6[0, 1, 2, 3, 4, 3, 2, 7, 8]
-    # 在位置1的时候，4
-    # 次交换把2 - 4
-    # 都换对了，所以直接跳到5，然后再换两次就有序了。
-    # 因为每个数字一开始都不在正确的位置上，所以交换次数很高，6
-    # 次是正确次数，实际上还会有第7次一个无用交换
-
+#
+# 将数组元素对应为索引的位置加n
+# 遍历加n后的数组，若数组元素值小于等于n，则说明数组下标值不存在，即消失的数字
+# class Solution {
+# public:
+#     vector<int> findDisappearedNumbers(vector<int>& nums) {
+#         vector<int> res;
+#         if(nums.empty()) return nums;
+#         for(int i=0;i<nums.size();i++)
+#         {
+#             int index=(nums[i]-1)%nums.size();
+#             nums[index]+=nums.size();
+#         }
+#         for(int i=0;i<nums.size();i++)
+#         {
+#             if(nums[i]<=nums.size())
+#                 res.push_back(i+1);
+#         }
+#         return res;
+#     }
+# };
 
 class Solution(object):
     def findDisappearedNumbers(self, nums):
@@ -63,3 +52,7 @@ class Solution(object):
             # 始终保持nums[index]为负数
             nums[index] = -abs(nums[index])
         return [i + 1 for i, num in enumerate(nums) if num > 0]
+
+    # 稍微解释下，最高赞的 @ 江南花家七公子的答案，遍历一遍nums数组，把每一位上的数组元素绝对值（因为有可能被之前的遍历标记为负数，但其绝对值依旧保留了原始的数组下标信息）当成数组下标，定位到属于这个数值元素的位置，这里注意一下错位对应，因为数组需要从0开始的。定位到属于该数组元素的位置后，利用令该位置上的值为负数进行标记，这里就可以无视重复的元素值带来的影响（不管重复几次，这个位置上还是负值）。最后，
+    # 没有出现在[1, n]
+    # 范围内的数字，他对应数组下标上的数组元素是不会被标记为负数的，再遍历一次就可以找到答案。 方法好牛逼，学到了。
