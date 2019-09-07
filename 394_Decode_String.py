e4ca2a2f2acb36091ffdace82518b1fbad9e0e69
changyuan <1936929110@qq.com>
2019-09-07 20:50:52 +0800

    ‘栈’

--- 394_Decode_String.py
+++ 394_Decode_String.py
@@ -20,3 +20,24 @@ class Solution:
                 result += i
 
         return result
+
+
+#dfs
+class Solution:
+    def decodeString(self, s: str) -> str:
+        def dfs(s, i):
+            res, multi = "", 0
+            while i < len(s):
+                if '0' <= s[i] <= '9':
+                    multi = multi * 10 + int(s[i])
+                elif s[i] == '[':
+                    i, tmp = dfs(s, i + 1)
+                    res += multi * tmp
+                    multi = 0
+                elif s[i] == ']':
+                    return i, res
+                else:
+                    res += s[i]
+                i += 1
+            return res
+        return dfs(s,0)
