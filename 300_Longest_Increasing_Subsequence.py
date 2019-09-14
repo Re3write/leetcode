dbd4b9007a3c1fdeabea3b314710772877ead532
changyuan <1936929110@qq.com>
2019-09-14 20:11:45 +0800

    ‘动态规划，最长递增子序列，‘二分法优化’’

--- 300_Longest_Increasing_Subsequence.py
+++ 300_Longest_Increasing_Subsequence.py
@@ -10,3 +10,26 @@ class Solution:
                 if nums[i] > nums[index]:
                     dp[i] = max(dp[i],dp[index]+1)
         return max(dp)
+
+##二分法
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        size = len(nums)
+        if size < 2:
+            return size
+
+        cell = [nums[0]]
+        for num in nums[1:]:
+            if num > cell[-1]:
+                cell.append(num)
+                continue
+
+            l, r = 0, len(cell) - 1
+            while l < r:
+                mid = l + (r - l) // 2
+                if cell[mid] < num:
+                    l = mid + 1
+                else:
+                    r = mid
+            cell[l] = num
+        return len(cell)
