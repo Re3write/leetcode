4924e5546f84024bb703416f779ebad753a0482e
Administrator <changyuan@ebupt.com>
2019-09-23 18:39:57 +0800

    ‘合并二叉树 递归’

--- 617_Merge_Two_Binary_Trees.py
+++ 617_Merge_Two_Binary_Trees.py
@@ -58,8 +58,13 @@ class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
         if not t1 and t2:
             return t2
+        elif not t2 and t1:
+            return t1
+        elif not t2 and not t1:
+            return None
         elif t1 and t2:
             t1.val = t2.val+t1.val
             t1.left = self.mergeTrees(t1.left,t2.left)
             t1.right = self.mergeTrees(t1.right,t2.right)
         return t1
+
