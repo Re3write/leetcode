db269e93b9d6ebafb17a07db00a6cc30064f3b99
Administrator <changyuan@ebupt.com>
2019-09-26 15:27:20 +0800

    ‘递归!!’

--- 543_Diameter_of_Binary_Tree.py
+++ 543_Diameter_of_Binary_Tree.py
@@ -1,12 +1,12 @@
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        self.max_length = 1
+        self.max_length = 0
         def preorder(root):
             if root == None:
                 return 0
             left = preorder(root.left)
             right = preorder(root.right)
-            self.max_length = max(self.max_length,left+right+1)
+            self.max_length = max(self.max_length,left+right)
             return max(left,right)+1
         preorder(root)
-        return self.max_length -1
+        return self.max_length
