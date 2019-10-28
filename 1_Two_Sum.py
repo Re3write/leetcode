82f06136ccde7cb85f1e24710b8b60314d126332
Administrator <changyuan@ebupt.com>
2019-10-28 15:38:53 +0800

    ‘在哈希中查找，数字作为key’

---
+++ 1_Two_Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+  def twoSum(self, nums, target):
+    hashmap={}
+    for ind,num in enumerate(nums):
+        hashmap[num] = ind
+    for i,num in enumerate(nums):
+        j = hashmap.get(target - num)
+        if j is not None and i!=j:
+            return [i,j]
