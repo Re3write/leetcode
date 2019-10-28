d33a6964a426d3e3ff39e446c92666b9efa2e392
Administrator <changyuan@ebupt.com>
2019-10-28 17:19:03 +0800

    ‘链表逐位相加’

---
+++ 2_Two_Sum_Add.py
@@ -0,0 +1,77 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 == None or l2 == None:
+            return None
+
+        def read_number(head):
+            number = []
+            while head:
+                number.append(head.val)
+                head = head.next
+            num = 0
+            for x in number[::-1]:
+                num = num * 10 + x
+            return num
+
+        number1 = read_number(l1)
+        number2 = read_number(l2)
+
+        number_list = str(number1 + number2)[::-1]
+        head = ListNode(int(number_list[0]))
+        node = head
+        for num in number_list[1:]:
+            node.next = ListNode(int(num))
+            node = node.next
+
+        return head
+
+
+
+##按数字方法每位加和连乘
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+        n = l1.val + l2.val
+        l3 = ListNode(n % 10)
+        l3.next = ListNode(n // 10)
+        p1 = l1.next
+        p2 = l2.next
+        p3 = l3
+        while True:
+            if p1 and p2:
+                sum = p1.val + p2.val + p3.next.val
+                p3.next.val = sum % 10
+                p3.next.next = ListNode(sum // 10)
+                p1 = p1.next
+                p2 = p2.next
+                p3 = p3.next
+            elif p1 and not p2:
+                sum = p1.val + p3.next.val
+                p3.next.val = sum % 10
+                p3.next.next = ListNode(sum // 10)
+                p1 = p1.next
+                p3 = p3.next
+            elif not p1 and p2:
+                sum = p2.val + p3.next.val
+                p3.next.val = sum % 10
+                p3.next.next = ListNode(sum // 10)
+                p2 = p2.next
+                p3 = p3.next
+            else:
+                if p3.next.val == 0:
+                    p3.next = None
+                break
+            return l3
+
