a954eab1a65715e826d031c95721aa97c39014d2
Administrator <changyuan@ebupt.com>
2019-11-05 18:06:53 +0800

    ‘DFS’

--- 494_Target_Sum.py
+++ 494_Target_Sum.py
@@ -43,3 +43,14 @@ class Solution:
                 if new_total == S and len(path + [i]) == len(nums):
                     count += 1
         return count
+
+
+#DFS
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        d = {}
+        def dfs(cur, i, d):
+            if i < len(nums) and (cur, i) not in d: # 搜索周围节点
+                d[(cur, i)] = dfs(cur + nums[i], i + 1, d) + dfs(cur - nums[i],i + 1, d)
+            return d.get((cur, i), int(cur == S))
+        return dfs(0, 0, d)
