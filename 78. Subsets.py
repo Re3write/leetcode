3a53ef12a76ad8157d5858d1733d9bf5d3da0103
Administrator <changyuan@ebupt.com>
2019-11-29 16:24:11 +0800

    ‘求子集，递归经典题目’

---
+++ 78. Subsets.py
@@ -0,0 +1,32 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        if not nums:
+            return []
+        def helper(nums, temp ,pos):
+            result.append(temp[:])
+            if not nums:
+                return
+            for i in range(pos,len(nums)):
+                 temp.append(nums[i])
+                 helper(nums[:i]+nums[i+1:],temp,i)
+                 temp.pop()
+        helper(nums,[],0)
+        return result
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        if not nums:
+            return []
+        def helper(nums, temp ,pos):
+            result.append(temp[:])
+            if pos == len(nums):
+                return
+            for i in range(pos,len(nums)):
+                 temp.append(nums[i])
+                 helper(nums,temp,i+1)
+                 temp.pop()
+        helper(nums,[],0)
+        return result
