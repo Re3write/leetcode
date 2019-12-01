5832461fcc312fdc6087cc1a784dd738c918503a
changyuan <changyuan@ebupt.com>
2019-12-01 11:10:02 +0800

    ‘二叉树遍历栈写法，颜色标记法’

---
+++ 94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,58 @@
+#递归写法
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        def helper(root):
+            if not root:
+                return
+            helper(root.left)
+            res.append(root.val)
+            helper(root.right)
+        helper(root)
+        return res
+
+# 因此，我在这里介绍一种“颜色标记法”（瞎起的名字……），兼具栈迭代方法的高效，又像递归方法一样简洁易懂，更重要的是，这种方法对于前序、中序、后序遍历，能够写出完全一致的代码。
+#
+# 其核心思想如下：
+#
+# 使用颜色标记节点的状态，新节点为白色，已访问的节点为灰色。
+# 如果遇到的节点为白色，则将其标记为灰色，然后将其右子节点、自身、左子节点依次入栈。
+# 如果遇到的节点为灰色，则将节点的值输出。
+# 使用这种方法实现的中序遍历如下：
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        WHITE, GRAY = 0, 1
+        res = []
+        stack = [(WHITE, root)]
+        while stack:
+            color, node = stack.pop()
+            if node is None: continue
+            if color == WHITE:
+                stack.append((WHITE, node.right))
+                stack.append((GRAY, node))
+                stack.append((WHITE, node.left))
+            else:
+                res.append(node.val)
+        return res
+
+#层次遍历，楼主的方法记忆很方便，前，中，后，层次遍历都可以统一起来，
+# 思路和时间与内存占用似乎和递归差不多？补充一下层次遍历的，加个level即可。
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        WHITE, GRAY = 0, 1
+        stack = []
+        init_level = 0
+        stack.append((root, WHITE, init_level))
+        result = []
+        while stack:
+            node, color, level = stack.pop()
+            if node:
+                if color == WHITE:
+                    stack.append((node.right, WHITE, level+1))
+                    stack.append((node.left, WHITE, level+1))
+                    stack.append((node, GRAY, level))
+                else:
+                    if len(result) == level: result.append([])
+                    result[level].append(node.val)
+        return result
