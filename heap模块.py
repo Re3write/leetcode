80245917e3b5ffe061b9490decfb3b6d67b4b412
changyuan <changyuan@ebupt.com>
2019-12-07 21:43:51 +0800

    'heap常用模块'

---
+++ heap模块.py
@@ -0,0 +1,8 @@
+# 模块heapq中一些重要的函数
+#                                                                                                                   
+#  heappush(heap, x)                                        将x压入堆中
+#  heappop(heap)                                      从堆中弹出最小的元素
+#  heapify(heap)                                           让列表具备堆特征
+#  heapreplace(heap, x)                            弹出最小的元素，并将x压入堆中
+#  nlargest(n, iter)                                       返回iter中n个最大的元素
+#  nsmallest(n, iter)                                   返回iter中n个最小的元素
