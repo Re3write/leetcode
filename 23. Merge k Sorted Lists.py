dcfe893722ce824818673d7fcb4b2005675df997
changyuan <changyuan@ebupt.com>
2019-12-08 11:30:49 +0800

    '合并k个链表，heapq，分治法'

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -35,22 +35,23 @@ class Solution:
 
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        if not lists:return
-        n = len(lists)
-        return self.merge(lists, 0, n-1)
-    def merge(self,lists, left, right):
-        if left == right:
-            return lists[left]
-        mid = left + (right - left) // 2
-        l1 = self.merge(lists, left, mid)
-        l2 = self.merge(lists, mid+1, right)
-        return self.mergeTwoLists(l1, l2)
-    def mergeTwoLists(self,l1, l2):
-        if not l1:return l2
-        if not l2:return l1
-        if l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
+        length = len(lists)
+        if length == 1:
+            return lists[0]
+        left = self.mergeKLists(lists[:length//2])
+        right = self.mergeKLists(lists[length//2:])
+        return self.mergeTwoLists(left,right)
+
+    def mergeTwoLists(self,l1,l2):
+        if not l1 and not l2:
+            return None
+        if not l1:
             return l2
+        if not l2:
+            return l1
+        if l1.val > l2.val:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
+        else:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
