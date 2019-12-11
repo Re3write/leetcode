7c70b2298801c8d3b9ca716550b0520780784ca2
changyuan <changyuan@ebupt.com>
2019-12-11 20:14:30 +0800

    '双指针找第一个匹配子串'

---
+++ 28. Implement strStr().py
@@ -0,0 +1,15 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        if not haystack:
+            return -1
+        start = 0
+        end = start
+        for i in range(len(haystack)):
+            if haystack[i] == needle[0]:
+                start = i
+                end = start + len(needle)
+                if haystack[start:end] == needle:
+                    return start
+        return -1
