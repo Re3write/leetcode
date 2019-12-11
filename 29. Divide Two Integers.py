1e7890574343271285e21d874175d9c5fcda2ed6
changyuan <changyuan@ebupt.com>
2019-12-11 21:14:02 +0800

    '二分法求两数除法，或者商加倍法，商加倍也是二分法的一种'

--- 29. Divide Two Integers.py
+++ 29. Divide Two Integers.py
@@ -13,3 +13,23 @@ class Solution:
                 low = middle + 1
             else:
                 return min(max(-2 ** 31, flag * middle), 2 ** 31 - 1)
+
+
+#或者每次让商加倍
+
+class Solution:
+    def divide(self, divd: int, dior: int) -> int:
+        res = 0
+        sign =  1 if divd ^ dior >= 0 else -1
+        #print(sign)
+        divd = abs(divd)
+        dior = abs(dior)
+        while divd >= dior:
+            tmp, i = dior, 1
+            while divd >= tmp:
+                divd -= tmp
+                res += i
+                i <<= 1
+                tmp <<= 1
+        res = res * sign
+        return min(max(-2**31, res), 2**31-1)
